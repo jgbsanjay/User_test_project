c86bcb44ca5c61a767cc56bd15752fb38afc200d
jgbsanjay <jgb.sanjay@gmail.com>
2020-06-25 15:27:11 -0400

    user commit

---
+++ src/User_package/UserClass.java
@@ -0,0 +1,15 @@
+package User_package;
+
+public class UserClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=15;
+		int j=5;
+		int k=i-j;
+		System.out.println("The difference is "+k);
+
+	}
+
+}
